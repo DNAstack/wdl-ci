5ec0922385f0002427f6538b9b0e583dfedcb5a6
Heather Ward <heather@dnastack.com>
2023-02-02 13:53:45 -0500

    Add custom linter handling required runtime arguments

--- src/wdlci/cli/lint.py
+++ src/wdlci/cli/lint.py
@@ -1,7 +1,32 @@
 import json
 import WDL
 import WDL.Lint
+from WDL.Lint import a_linter, Linter
 from wdlci.config import Config
+from typing import Any
+
+
+@a_linter
+class MissingRuntimeKey(Linter):
+    required_keys = set(
+        [
+            "docker",
+            "cpu",
+            "memory",
+            "disk",
+            "disks",
+            "preemptible",
+            "maxRetries",
+            "awsBatchRetryAttempts",
+            "queueArn",
+            "zones",
+        ]
+    )
+
+    def task(self, obj: WDL.Tree.Task) -> Any:
+        for k in self.required_keys:
+            if k not in obj.runtime:
+                self.add(obj, "Missing required runtime parameter: " + k)
 
 
 def lint_handler(kwargs):
@@ -16,19 +41,19 @@ def lint_handler(kwargs):
         for (pos, lint_class, message, suppressed) in lint:
             assert isinstance(pos, WDL.SourcePosition)
             assert isinstance(lint_class, str) and isinstance(message, str)
-            # if not suppressed:
-            print(
-                json.dumps(
-                    {
-                        "uri": pos.uri,
-                        "abspath": pos.abspath,
-                        "line": pos.line,
-                        "end_line": pos.end_line,
-                        "column": pos.column,
-                        "end_column": pos.end_column,
-                        "lint": lint_class,
-                        "message": message,
-                    }
+            if not suppressed:
+                print(
+                    json.dumps(
+                        {
+                            "uri": pos.uri,
+                            "abspath": pos.abspath,
+                            "line": pos.line,
+                            "end_line": pos.end_line,
+                            "column": pos.column,
+                            "end_column": pos.end_column,
+                            "lint": lint_class,
+                            "message": message,
+                        }
+                    )
                 )
-            )
         print()
