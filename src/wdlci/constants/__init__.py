9950f646d0b39f6af2c52621fe70d60a1baa6cba
jb-adams <jeremy@dnastack.com>
2023-02-16 10:49:36 -0500

    remove default URLs/client ID

--- src/wdlci/constants/__init__.py
+++ src/wdlci/constants/__init__.py
@@ -4,10 +4,3 @@ CONFIG_JSON = "wdl-ci.config.json"
 CHANGES_JSON = ".wdltest.changes.json"
 TESTS_JSON = "tests.json"
 SUBMISSION_JSON = ".wdltest.submission.json"
-
-DEFAULT_WALLET_URL = "https://wallet.gcp-managed-production.dnastack.com"
-DEFAULT_WALLET_CLIENT_ID = "workbench-public"
-DEFAULT_WORKBENCH_EWES_URL = "https://ewes-service.workbench.dnastack.com"
-DEFAULT_WORKBENCH_WORKFLOW_SERVICE_URL = (
-    "https://workflow-service.workbench.dnastack.com"
-)
