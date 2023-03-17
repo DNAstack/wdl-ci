e74a6e2348204d3178a36be9264c5c9f3823a60c
Heather Ward <heather@dnastack.com>
2023-03-17 11:17:54 -0400

    Recursively set parameters for array and dict type inputs/outputs
    [#184654789]

--- src/wdlci/utils/hydrate_params.py
+++ src/wdlci/utils/hydrate_params.py
@@ -4,12 +4,28 @@ class HydrateParams(object):
         result = {}
         separator = "" if workflow_name == "" else "."
 
-        for tk in target.keys():
-            tv = target[tk]
-            if type(tv) is str:
+        def _replace_value_recursive(target_value):
+            if type(target_value) is str:
                 for sk in source.keys():
                     sv = source[sk]
-                    tv = tv.replace("${" + sk + "}", sv)
+                    target_value = target_value.replace("${" + sk + "}", sv)
+                return target_value
+            elif type(target_value) is list:
+                substituted_list = list()
+                for item in target_value:
+                    substituted_list.append(_replace_value_recursive(item))
+                return substituted_list
+            elif type(target_value) is dict:
+                substituted_dict = dict()
+                for key, value in target_value.items():
+                    substituted_dict[key] = _replace_value_recursive(value)
+                return substituted_dict
+            else:
+                raise SystemExit(f"Unexpected type detected for value {value}")
+
+        for tk in target.keys():
+            tv = target[tk]
+            tv = _replace_value_recursive(tv)
 
             new_key = f"{workflow_name}{separator}{tk}" if update_key else tk
             result[new_key] = tv
