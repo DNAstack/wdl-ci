132e1bfa2c84ff526a800a9c792e17a72bbb46d7
Heather <heather@dnastack.com>
2023-03-30 06:57:52 -0700

    Update succeeded tasks (#17)
    * Update config on task success

--- src/wdlci/cli/monitor.py
+++ src/wdlci/cli/monitor.py
@@ -2,6 +2,8 @@ import jsonpickle
 import os
 import sys
 import time
+import re
+import WDL
 from wdlci.auth.refresh_token_auth import RefreshTokenAuth
 from wdlci.constants import *
 from wdlci.config import Config
@@ -88,19 +90,34 @@ def monitor_handler(kwargs):
         #   - completed successfully
         #   - have outputs that match the expected
         fail_n = 0
+        task_status = dict()
         for workflow_run in submission_state.workflow_runs:
+            # If a task fails during any of its runs, task_status[workflow_path][task]["succeeded"] will be updated to False
+            # If all tests succeed for this task, it will be set to True
+            task = workflow_run._task_key.key
+            test_index = workflow_run._test_i
+            workflow_path = re.sub(
+                rf"-{task}-{test_index}$", "", workflow_run._workflow_key
+            )
+            if workflow_path in task_status:
+                if task not in task_status[workflow_path]:
+                    task_status[workflow_path][task] = {"succeeded": None}
+            else:
+                task_status[workflow_path] = {task: {"succeeded": None}}
+
             if (
                 workflow_run.status == SubmissionStateWorkflowRun.STATUS_FINISH_SUCCESS
                 and workflow_run.validation_status
                 == SubmissionStateWorkflowRun.VALIDATION_SUCCESS
             ):
-                # success condition
-                pass
+                if task_status[workflow_path][task]["succeeded"] != False:
+                    task_status[workflow_path][task]["succeeded"] = True
             elif workflow_run.status == SubmissionStateWorkflowRun.STATUS_FINISH_FAIL:
                 fail_n += 1
                 print(
                     f"run with id '{workflow_run.wes_run_id}' failed execution with a state of {workflow_run.wes_state}"
                 )
+                task_status[workflow_path][task]["succeeded"] = False
             elif (
                 workflow_run.status == SubmissionStateWorkflowRun.STATUS_FINISH_SUCCESS
                 and workflow_run.validation_status
@@ -110,14 +127,43 @@ def monitor_handler(kwargs):
                 print(
                     f"run with id '{workflow_run.wes_run_id}' failed output validation. message: {workflow_run.validation_message}"
                 )
+                task_status[workflow_path][task]["succeeded"] = False
             else:
                 fail_n += 1
                 print(
                     f"run with id '{workflow_run.wes_run_id}' failed with an unspecified error"
                 )
+                task_status[workflow_path][task]["succeeded"] = False
 
         submission_state_encoded = jsonpickle.encode(submission_state)
         open(SUBMISSION_JSON, "w").write(submission_state_encoded)
+
+        # Update digests in config file for tasks that succeeded if --update-digests is set to true
+        config_updated = False
+        update_digests = kwargs["update_digests"]
+        if update_digests:
+            for workflow_path, tasks in task_status.items():
+                doc = WDL.load(workflow_path)
+                for task in doc.tasks:
+                    task_name = task.name
+                    if tasks.get(task_name):
+                        if tasks[task_name]["succeeded"]:
+                            print(
+                                f"All tests succeeded for [{workflow_path} - {task_name}]. Updating task digest."
+                            )
+                            task_digest = task.digest
+                            config.file.workflows[workflow_path].tasks[
+                                task_name
+                            ].digest = task_digest
+                            config_updated = True
+                        else:
+                            print(
+                                f"At least one test failed for [{workflow_path} - {task_name}]."
+                            )
+
+        if config_updated:
+            config.write()
+            print(f"Wrote updated config to [{CONFIG_JSON}]")
 
         if fail_n > 0:
             raise WdlTestCliExitException(
@@ -125,7 +171,6 @@ def monitor_handler(kwargs):
                 1,
             )
 
-        # TODO add successful finish message
         print(
             "Monitoring and validation complete. All workflow runs completed successfully and output the expected results"
         )
