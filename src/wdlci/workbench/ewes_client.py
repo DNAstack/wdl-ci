a244cb62cb6effd57a1e035bfc78be312b53ed80
Heather Ward <heather@dnastack.com>
2023-04-21 12:58:12 -0400

    Print stderr from failed tasks

--- src/wdlci/workbench/ewes_client.py
+++ src/wdlci/workbench/ewes_client.py
@@ -29,7 +29,9 @@ class EwesClient(object):
 
         headers = {"Authorization": "Bearer " + self.ewes_auth.access_token}
 
-        output_test_task_params = {k: workflow_run._outputs[k]['value'] for k in workflow_run._outputs.keys()}
+        output_test_task_params = {
+            k: workflow_run._outputs[k]["value"] for k in workflow_run._outputs.keys()
+        }
 
         form_data = {
             "workflow_url": f"{env.workbench_workflow_service_url}/{env.workbench_namespace}/workflows/{workflow_run._workflow_id}/versions/v1_0_0/descriptor",
@@ -70,6 +72,38 @@ class EwesClient(object):
                 workflow_run.finish_fail()
             elif wes_state in set(["COMPLETE"]):
                 workflow_run.finish_success()
+
+    def _print_stderr(self, stderr_url, headers, task_name):
+        terminal_format_bold = "\033[1m"
+        terminal_format_end = "\033[0m"
+        print(
+            f"{terminal_format_bold}═════ stderr ═════════════════════════════════════════ [{task_name}]{terminal_format_end}"
+        )
+        print()
+        response = requests.get(stderr_url, headers=headers)
+        print(response.content.decode("ascii"))
+        print(
+            f"{terminal_format_bold}═══════════════════════════════════════════════════════{terminal_format_end}"
+        )
+        print()
+
+    def get_failed_task_logs(self, workflow_run):
+        env = Config.instance().env
+        base_url, namespace = env.workbench_ewes_url, env.workbench_namespace
+        url = (
+            f"{base_url}/{namespace}/ga4gh/wes/v1/runs/{workflow_run.wes_run_id}/tasks"
+        )
+        headers = {"Authorization": "Bearer " + self.ewes_auth.access_token}
+
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            for task in response.json()["tasks"]:
+                if task["state"] == "EXECUTOR_ERROR":
+                    task_name = task["pretty_name"].split(".")[1]
+                    print(
+                        f"EXECUTOR_ERROR for [{workflow_run._workflow_key} - {task_name}]."
+                    )
+                    self._print_stderr(task["stderr"], headers, task_name)
 
     def __get_url(self):
         return Config.instance().workbench_ewes_url
