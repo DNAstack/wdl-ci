3ee098b7054aad8b45dee742d9f6407106012662
geneticsjesse <jw64450@gmail.com>
2024-03-05 16:30:34 -0500

    Remove check and append to list for tests with no tasks

--- src/wdlci/cli/detect_changes.py
+++ src/wdlci/cli/detect_changes.py
@@ -15,17 +15,12 @@ def detect_changes_handler(kwargs):
 
         changeset = Changeset()
 
-        tasks_without_tests = []
-
         for workflow, workflow_config in config.file.workflows.items():
             doc = WDL.load(workflow)
 
             for task in doc.tasks:
                 task_name = task.name
                 task_digest = task.digest
-
-                if not workflow_config.tasks[task_name].tests:
-                    tasks_without_tests.append(task_name)
 
                 if task_name in workflow_config.tasks:
                     previous_digest = workflow_config.tasks[task_name].digest
@@ -40,7 +35,6 @@ def detect_changes_handler(kwargs):
                     print(f"New task detected [{workflow} - {task_name}]")
                     workflow_change = changeset.add_workflow_change(workflow)
                     workflow_change.add_task_change(task_name)
-        print(f"The following tasks have no tests: {', '.join(tasks_without_tests)}")
 
         if len(changeset.workflow_changes) == 0:
             print("No new or modified tasks detected")
