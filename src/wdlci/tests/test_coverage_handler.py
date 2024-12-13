e243224693557b9730d5c071dd1397b2ba137674
geneticsjesse <jesse@dnastack.com>
2024-12-13 11:20:53 -0500

    Add tests for coverage handler -- in dev

---
+++ src/wdlci/tests/test_coverage_handler.py
@@ -0,0 +1,173 @@
+import unittest
+import os
+import subprocess
+import json
+
+from src.wdlci.cli.coverage import coverage_handler, coverage_summary
+from src.wdlci.config import Config
+
+
+class TestCoverageHandler(unittest.TestCase):
+    EXAMPLE_WDL_WORKFLOW = """version 1.0
+
+struct Reference {
+  File fasta
+  String organism
+}
+
+workflow call_variants {
+  input {
+    File bam
+    Reference ref
+  }
+
+  call freebayes {
+    input:
+      bam=bam,
+      ref=ref
+  }
+
+  output {
+    File vcf = freebayes.vcf
+  }
+}
+
+task freebayes {
+  input {
+    File bam
+    Reference ref
+    Float? min_alternate_fraction
+  }
+
+  String prefix = basename(bam, ".bam")
+  Float default_min_alternate_fraction = select_first([min_alternate_fraction, 0.2])
+
+  command <<<
+  freebayes -v '~{prefix}.vcf' -f ~{ref.fasta} \
+    -F ~{default_min_alternate_fraction} \
+    ~{bam}
+  >>>
+
+  runtime {
+    docker: "quay.io/biocontainers/freebayes:1.3.2--py36hc088bd4_0"
+  }
+
+  output {
+    File vcf = "${prefix}.vcf"
+  }
+}
+"""
+
+    def setUp(self):
+        # Create the WDL files with different workflow names
+        wdl_workflow_1 = self.EXAMPLE_WDL_WORKFLOW.replace(
+            "call_variants", "call_variants_1"
+        )
+        wdl_workflow_2 = self.EXAMPLE_WDL_WORKFLOW.replace(
+            "call_variants", "call_variants_2"
+        )
+
+        with open("test_call-variants_1.wdl", "w") as f:
+            f.write(wdl_workflow_1)
+        with open("test_call-variants_2.wdl", "w") as f:
+            f.write(wdl_workflow_2)
+        # Run the wdl-ci generate-config subcommand
+        subprocess.run(
+            ["wdl-ci", "generate-config"], check=True, stdout=subprocess.DEVNULL
+        )
+
+    def tearDown(self):
+        # Remove the WDL files
+        if os.path.exists("test_call-variants_1.wdl"):
+            os.remove("test_call-variants_1.wdl")
+        if os.path.exists("test_call-variants_2.wdl"):
+            os.remove("test_call-variants_2.wdl")
+        if os.path.exists("wdl-ci.config.json"):
+            os.remove("wdl-ci.config.json")
+
+    # def reset_config(self):
+    #     Config._cli_kwargs = None
+    #     Config._instance = None
+
+    def update_config_with_tests(self, wdl_1_tests, wdl_2_tests):
+        # Read the existing config file
+        with open("wdl-ci.config.json", "r") as f:
+            config = json.load(f)
+
+        config["workflows"]["test_call-variants_1.wdl"]["tasks"]["freebayes"][
+            "tests"
+        ] = wdl_1_tests
+        config["workflows"]["test_call-variants_2.wdl"]["tasks"]["freebayes"][
+            "tests"
+        ] = wdl_2_tests
+
+        # Write the updated config back to the file
+        with open("wdl-ci.config.json", "w") as f:
+            json.dump(config, f, indent=2)
+
+    def reset_coverage_summary(self):
+        coverage_summary["untested_workflows"] = []
+        coverage_summary["untested_tasks"] = {}
+        coverage_summary["untested_outputs"] = {}
+        coverage_summary["untested_outputs_with_optional_inputs"] = {}
+        coverage_summary["tested_outputs_dict"] = {}
+        coverage_summary["total_output_count"] = 0
+        coverage_summary["all_tests_list"] = []
+        coverage_summary["skipped_workflows"] = []
+
+    def test_identical_output_names(self):
+        # Reset the coverage_summary
+        self.reset_coverage_summary()
+
+        # Update the "tests" list for specific workflows
+        test_cases = [
+            {
+                "inputs": {
+                    "bam": "test.bam",
+                    "ref": {"fasta": "test.fasta", "organism": "test_organism"},
+                },
+                "output_tests": {
+                    "vcf": {
+                        "value": "test.vcf",
+                        "test_tasks": ["compare_file_basename"],
+                    }
+                },
+            }
+        ]
+        self.update_config_with_tests(wdl_1_tests=test_cases, wdl_2_tests=test_cases)
+
+        # Call the coverage_handler function
+        kwargs = {"target_coverage": None, "workflow_name": None}
+        coverage_handler(kwargs)
+
+        # Assertions
+        self.assertIn("compare_float", coverage_summary["untested_workflows"])
+        self.assertNotIn("call_variants_1", coverage_summary["untested_workflows"])
+        self.assertNotIn("call_variants_2", coverage_summary["untested_workflows"])
+        self.assertNotIn("freebayes", coverage_summary["untested_tasks"])
+        self.assertIn(
+            "vcf",
+            coverage_summary["untested_outputs_with_optional_inputs"][
+                "call_variants_1"
+            ]["freebayes"],
+        )
+        self.assertIn(
+            "vcf",
+            coverage_summary["tested_outputs_dict"]["call_variants_1"]["freebayes"],
+        )
+        self.assertIn(
+            "vcf",
+            coverage_summary["tested_outputs_dict"]["call_variants_2"]["freebayes"],
+        )
+
+    # def test_no_tasks_in_workflow(self):
+    #     # Update the "tests" list for specific workflows
+    #     test_cases = []
+    #     self.update_config_with_tests(wdl_1_tests=test_cases, wdl_2_tests=test_cases)
+    #     # Call the coverage_handler function
+    #     kwargs = {"target_coverage": None, "workflow_name": None}
+    #     coverage_handler(kwargs)
+
+
+if __name__ == "__main__":
+    unittest.main()
